8fc8951d0e4983e09adb6afb9caafded910951ff
Natallia-Karatava <nkaratova@gmail.com>
2025-02-25 16:21:25 +0100

    update

--- src/components/FooterComponent.jsx
+++ src/components/FooterComponent.jsx
@@ -1,4 +1,12 @@
 const FooterComponent = () => {
-  return <div>FooterComponent</div>;
+  return (
+    <footer className="footer sm:footer-horizontal footer-center bg-base-300 text-base-content p-4">
+      <aside>
+        <p>
+          Copyright © {new Date().getFullYear()} - All right reserved by Dev
+        </p>
+      </aside>
+    </footer>
+  );
 };
 export default FooterComponent;
