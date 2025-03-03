f61a93ea09fb0ea068b978b06fafcf80f020bfd9
Natallia-Karatava <nkaratova@gmail.com>
2025-03-03 12:12:16 +0100

    MealsPage styling

--- src/page/MealsPage.jsx
+++ src/page/MealsPage.jsx
@@ -22,13 +22,20 @@ const MealsPage = () => {
   }, []);
 
   return (
-    <div className="container mx-auto p-4">
-      {meals.length > 0 ? (
-        meals.map((food) => <CardItemComponent key={food.idMeal} food={food} />)
-      ) : (
-        <p>Kein Essen is da </p>
-      )}
-    </div>
+    <>
+      <h1 className="text-4xl font-bold text-center dark:text-yellow-500 text-slate-900 pb-4 pt-4">
+        Geheime Rezepte Lager
+      </h1>
+      <div className="flex flex-wrap gap-4 justify-center">
+        {meals.length > 0 ? (
+          meals.map((food) => (
+            <CardItemComponent key={food.idMeal} food={food} />
+          ))
+        ) : (
+          <p>Kein Essen is da </p>
+        )}
+      </div>
+    </>
   );
 };
 export default MealsPage;
